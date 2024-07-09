d108e028d6b84735d6b2d55a6db5e94160052fc7
Halil Branica <halilbranica@gmail.com>
2024-07-09 10:50:28 +0100

    stopped eager loading with files. shitty code

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,7 +34,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user() ? new UserResource($request->user()->with('files')->first()) : null,
+                'user' => $request->user() ? new UserResource($request->user()) : null,
             ],
         ];
     }
